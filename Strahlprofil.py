aa799bb2e593fbe72e6d57113d9eb33ad38b234e
corinna.wegner <91503624+corinnawegner@users.noreply.github.com>
2022-06-09 22:39:25 +0200

    Fitting Data to gaussian distribution
    Only the beginning of the code

---
+++ Strahlprofil.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  9 22:27:53 2022
+
+@author: corin
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+csv_path = r""
+data = pd.read_csv(csv_path, delimiter=";")
