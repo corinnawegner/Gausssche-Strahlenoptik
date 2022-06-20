5ddcb590c6ce149a5791313567a18ba7b40a2585
GarenGregorian <G.Gregorian@physik.uni-muenchen.de>
2022-06-20 14:35:02 +0100

    Git fixed by corinna

--- Strahlprofil_a.py
+++ Strahlprofil_a.py
@@ -8,9 +8,7 @@ Created on Thu Jun  9 22:27:53 2022
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from scipy.optimize import curve_fit
-import scipy.integrate as integrate
-from functools import partial #https://stackoverflow.com/questions/61675014/integral-with-variable-limits-in-python
+from scipy import stats
 
 csv_path_a = r"C:/Users/corin/Gausssche-Strahlenoptik/Strahlprofil_a.csv"
 csv_path_b = r"C:/Users/corin/Gausssche-Strahlenoptik/Strahlprofil_b.csv" # Vielleicht Teil b in eigenen Code auslagern 
@@ -20,78 +18,18 @@ a_45 = pd.read_csv(csv_path_a, delimiter=";", header=None, skiprows=6, nrows=2, 
 a_105 = pd.read_csv(csv_path_a, delimiter=";", header=None, skiprows=9, nrows=2, usecols=[1,2,3,4,5,6])
 data_a = [a_45, a_72, a_105]
 
-h = 6.62607015*10**(-34)
-c = 299792458
-wvl = 632.8*10**(-9)
-e = 1.602176634*10**(-19)
-distLS = 15.5 #Distance lens-fibre end (Source of laser beam)
-Rd = 10**4 #Resistance of the photodiode in ohm
+distLS = 15.5 #Distance lense-fibre end (Source of laser beam)
 
-#normalized micrometer positions:
-x72 = a_72.values[0]-a_72.values[0][0] 
-x72new = a_72.values[0]-a_72.values[0][2]
-x45 = a_45.values[0]-a_45.values[0][0]
-x105 = a_105.values[0]-a_105.values[0][0]
-
-maxints = []
-omegas = []
-
-def P(U, R):
-    return (h*c*U)/(wvl*R*e*0.75)
-
-def gaussint(x, I0, w):
-    inner = lambda xp: np.exp((-2*xp**2)/(w**2))
-    #inteqrate.quad kann keine Integrationsgrenzen als Variablen haben, darum kompliziertere Lösung:
-    integral = np.array(list(map(partial(integrate.quad, inner, b=np.inf), x)))[:,0]
-    return I0*integral
-
-def rayleigh(w):
-    return (np.pi*w**2)/wvl
-
-plt.plot(x72, P(a_72.values[1], Rd), 'ro', label="8,3 +- 0,2cm")
-plt.plot(x45, P(a_45.values[1], Rd), 'bo', label="11,0 +- 0,2 cm")
-plt.plot(x105, P(a_105.values[1], Rd), 'go', label="5,0 +- 0,2 cm")
-
-#8,3cm fit
-popt, cov = curve_fit(gaussint, x72, P(a_72.values[1], Rd))
-maxintensity, omega = popt
-plt.plot(x72, gaussint(x72, maxintensity, omega), 'r--') #Die ersten beiden Werte sind schlecht
-popt, cov = curve_fit(gaussint, x72new[2:], P((a_72.values[1]), Rd)[2:])
-maxintensity, omega = popt
-maxints.append(maxintensity)
-omegas.append(omega)
-plt.plot(x72[2:], gaussint(x72new[2:], maxintensity, omega), 'r')
-print("8,3cm:\n", "I_0:", maxintensity, "Strahltaille:", omega)
-
-#11cm fit
-popt, cov = curve_fit(gaussint, x45, P(a_45.values[1], Rd))
-maxintensity, omega = popt
-#maxints.append(maxintensity)
-#omegas.append(omega)
-plt.plot(x45, gaussint(x45, maxintensity, omega), 'b')
-print("11cm:\n", "I_0:", maxintensity, "Strahltaille:", omega)
-
-#5,0cm fit
-popt, cov = curve_fit(gaussint, x105, P(a_105.values[1], Rd))
-maxintensity, omega = popt
-maxints.append(maxintensity)
-omegas.append(omega)
-plt.plot(x105, gaussint(x105, maxintensity, omega), 'g')
-print("5,0cm:\n", "I_0:", maxintensity, "Strahltaille:", omega)
-
-plt.xlabel("x-x_0 in mm (normalized micrometer position)")
-plt.ylabel("power in mW") #Milli weil die Spannungen in mV angegeben sind
+plt.plot(a_72.values[0]-a_72.values[0][0], a_72.values[1], 'ro', label="8,3cm")
+plt.plot(a_45.values[0]-a_45.values[0][0], a_45.values[1], 'bo', label="11,0 cm")
+plt.plot(a_105.values[0]-a_105.values[0][0], a_105.values[1],'go', label="5,0 cm")
+plt.xlabel("normalized micrometer position")
+plt.ylabel("voltage in mV")
 plt.legend(title="Distance razor blade - fibre end")
-plt.title("cross section profile of collimated beam")
-plt.savefig("part a: cross section profile of collimated beam.png", dpi=400)
-
-print("waist: (", np.mean(omegas), "+-", np.std(omegas), ") mm")
-rays = [rayleigh(i)/10**6 for i in omegas] #10**6 weil millimeter umrechnen
-print("rayleigh length: (", np.mean(rays), "+-", np.std(rays), ") mm")
-
 #Muss noch y-Werte in Lichtleistungen umrechnen (und ylabel anpassen!)
+#Fehler?
 #Aufgaben Seite 14 und 15
 #Fehler bei 8,3 cm: Wahrscheinlich nicht als ersten Wert die stelle gewählt, an der die Rasierklinge
 #gerade noch so nicht in den strahl hineinragt, sondern schon früher gemessen (rasierklinge war noch lange nicht am strahl dran)
-#Offset durch Hintergrundhelligkeit abziehen: wurde gemacht durch normalisierte spannung
-# 11,0 cm : omega negativ... wahrscheinlich zu große entfernung von kabelausgang, sodass fehler wahrscheinlicher werden
+
+# def lichtleistung(U):
--- Strahlprofil_b.py
+++ Strahlprofil_b.py
@@ -5,18 +5,10 @@ Created on Sat Jun 11 00:53:58 2022
 @author: corin
 """
 
-# Task: d bestimmen Sie aus dem Verlauf von w(z) den
-#Waist w0 dieses Strahls mithilfe eines Fit-Programms. Dabei passen Sie die Funktion von w(z)
-#nach Gl. (11) an die gemessenen Werte w1(z1), w2(z2),... an. Die Position des waist z0 ist dabei
-#ein unbekannter Parameter, welcher gleichzeitig angepasst werden muss. Bestimmen Sie aus
-#dem Waist w0 auch die Rayleigh-Länge zR
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from scipy.optimize import curve_fit
-import scipy.integrate as integrate
-from functools import partial
+from scipy import stats
 
 csv_path_b = r"C:/Users/corin/Gausssche-Strahlenoptik/Strahlprofil_b.csv"
 
@@ -27,93 +19,13 @@ b_6 = pd.read_csv(csv_path_b, delimiter=";", header=None, skiprows=12, nrows=2, 
 b_3 = pd.read_csv(csv_path_b, delimiter=";", header=None, skiprows=15, nrows=2, usecols=[1,2,3,4,5,6])
 b_14 = pd.read_csv(csv_path_b, delimiter=";", header=None, skiprows=18, nrows=2, usecols=[1,2,3,4,5,6]) #14,3cm
 b_17 = pd.read_csv(csv_path_b, delimiter=";", header=None, skiprows=21, nrows=2, usecols=[1,2,3,4,5,6])
-data = [b_3, b_6, b_9, b_10, b_11, b_14, b_17]
 
-Rd = 10*10**3
-#f = 100 #mm
+plt.plot(b_10.values[0]-b_10.values[0][0], b_10.values[1], 'ro', label="10,0 cm")
+plt.plot(b_9.values[0]-b_9.values[0][0], b_9.values[1], 'bo', label="9,0 cm")
+plt.plot(b_11.values[0]-b_11.values[0][0], b_11.values[1], 'go', label="11,0 cm")
+plt.plot(b_6.values[0]-b_6.values[0][0], b_6.values[1], 'yo', label="6,0 cm")
+plt.plot(b_3.values[0]-b_3.values[0][0], b_3.values[1], 'co', label="3,0 cm")
+plt.plot(b_14.values[0]-b_14.values[0][0], b_14.values[1], 'mo', label="14,0 cm")
+plt.plot(b_17.values[0]-b_17.values[0][0], b_17.values[1], 'ko', label="17,0 cm")
 
-maxints=[]
-omegas=[]
-zvals=[0,-1,1,-4,-7, 4]
-#zvals=[10**(-6)*i for i in zvals]
-zvals.sort()
-localwaists=[]
-
-def P(U, R):
-    h = 6.62607015*10**(-34)
-    c = 299792458
-    wvl = 632.8*10**(-9)
-    e = 1.602176634*10**(-19)
-    return (h*c*U)/(wvl*R*e*0.75)
-
-def localwaist(z, omega_0, rayleigh):
-    #rayleigh = np.pi*(omega_0**2)/(632.8*10**(-9)
-    return omega_0*np.sqrt(1+(z**2)/(rayleigh**2))
-
-#lokaler Strahlradius bestimmen
-def gaussint(x, I0, w):
-    inner = lambda xp: np.exp((-2*xp**2)/(w**2))
-    #inteqrate.quad kann keine Integrationsgrenzen als Variablen haben, darum kompliziertere Lösung:
-    integral = np.array(list(map(partial(integrate.quad, inner, b=np.inf), x)))[:,0]
-    return I0*integral
-
-for d in data:
-    popt, cov = curve_fit(gaussint, d.values[0]-d.values[0][0] , P(d.values[1], Rd))
-    maxintensity, omega = popt
-    maxints.append(maxintensity)
-    omegas.append(omega)
-    print("I_0:", maxintensity, "Strahltaille:", omega)
-
-#nahe z=0
-#indizierung von maxints und omegas potentielle fehlerquelle
-plt.plot(b_10.values[0]-b_10.values[0][0], P(b_10.values[1], Rd), 'ro', label="0,0 cm")
-plt.plot(b_9.values[0]-b_9.values[0][0], P(b_9.values[1], Rd), 'bo', label="-1,0 cm")
-plt.plot(b_11.values[0]-b_11.values[0][0], P(b_11.values[1], Rd), 'go', label="1,0 cm")
-plt.legend()
-plt.plot(b_10.values[0]-b_10.values[0][0], gaussint(b_10.values[0]-b_10.values[0][0], maxints[3], omegas[3]), 'r')
-plt.plot(b_9.values[0]-b_9.values[0][0], gaussint(b_9.values[0]-b_9.values[0][0], maxints[2], omegas[2]), 'b')
-plt.plot(b_11.values[0]-b_11.values[0][0], gaussint(b_11.values[0]-b_11.values[0][0], maxints[4], omegas[4]), 'g')
-plt.xlabel("x-x_0 in mm")
-plt.ylabel("Power in W") 
-plt.title("Cross section profile focused beam (near focal point)")
-plt.savefig("Cross section profile focused beam (near focal point).png", dpi=400)
-plt.clf()
-#große z
-plt.plot(b_6.values[0]-b_6.values[0][0], P(b_6.values[1], Rd), 'yo', label="-4,0 cm")
-plt.plot(b_3.values[0]-b_3.values[0][0], P(b_3.values[1], Rd), 'co', label="-7,0 cm")
-plt.plot(b_14.values[0]-b_14.values[0][0], P(b_14.values[1], Rd), 'mo', label="4,3 cm")
-plt.plot(b_17.values[0]-b_17.values[0][0], P(b_17.values[1], Rd), 'ko', label="7,0 cm")
-plt.legend()
-plt.plot(b_6.values[0]-b_6.values[0][0], gaussint(b_6.values[0]-b_6.values[0][0], maxints[1], omegas[1]), 'y')
-plt.plot(b_3.values[0]-b_3.values[0][0], gaussint(b_3.values[0]-b_3.values[0][0], maxints[0], omegas[0]), 'c')
-plt.plot(b_14.values[0]-b_14.values[0][0], gaussint(b_14.values[0]-b_14.values[0][0], maxints[5], omegas[5]), 'm')
-plt.plot(b_17.values[0]-b_17.values[0][0], gaussint(b_17.values[0]-b_17.values[0][0], maxints[6], omegas[6]), 'k')
-plt.xlabel("x-x_0 in mm")
-plt.ylabel("Power in W") 
-plt.title("Cross section profile focused beam (far from focal point)")
-plt.savefig("Cross section profile focused beam (far from focal point).png", dpi=400)
-plt.clf()
-#
-##waist bestimmen
-##Fehlerquelle: Mehr Messungen wären nötig
-#Fehlerquellen aus H3 Notes
-## b_17 liefert negativen waist ??? Vielleicht am ungenausten weil am weitesten von Quelle weg-> am meisten einfluss von störfaktoren
-# dran denken 14,3 cm
-popt, err = curve_fit(localwaist, zvals, omegas[:6], absolute_sigma="True") #fitted localwaist an die z-Werte zusammen mit den dazugehörigen omegas
-waist, zR = popt
-plt.plot(zvals, omegas[:6], 'ro', label="data")
-for i in zvals:
-    localwaists.append(localwaist(i, waist, zR)) #nur weil plt.plot(zvals, localwaist(zvals)) nicht funktioniert
-plt.plot(zvals, localwaists, 'b', label = "fit")
-plt.vlines(0, 0, 1.7, 'y', '--', label="focal position")
-plt.xlabel("z in cm")
-plt.ylabel("omega(z) in mm")
-plt.legend()
-title = "beam waist against razor position from focal point"
-plt.title(title)
-plt.savefig("beam waist against razor position from focal point.png", dpi=400)
-
-print("waist: (", waist, "+-", np.sqrt(err[0,0]), ") mm")
-print("rayleigh length: (", zR, "+-", np.sqrt(err[1,1]), ") mm") 
-# standardabweichung größer als  eigentlicher wert bei rayleigh length
-#waist mit 0.8 mm wahrscheinlich zu groß, weit über dem gemessenen minimum (siehe figure letzte)
+# Zur Übersichtlichkeit: near focal point in extra plot?
